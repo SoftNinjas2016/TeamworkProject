24a5fba5a910707fac2ec2e60cf1437ae054ea37
laze1313 <laze_13@abv.bg>
2016-08-31 15:48:49 +0300

    label notification

--- MVC-Blog-ALV/MVC-Blog-ALV/App_Start/BundleConfig.cs
+++ MVC-Blog-ALV/MVC-Blog-ALV/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace MVC_Blog_ALV
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ajax").Include(
+                      "~/Scripts/jquery.unobtrusive-ajax.min.js"));
         }
     }
 }
